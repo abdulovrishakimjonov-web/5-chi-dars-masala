bed89a3df5d547fe268201267e927bcfd651d412
Your Name <your.email@example.com>
2025-09-14 04:54:11 +0500

    5-dars-masala

---
+++ main.js
@@ -0,0 +1,22 @@
+// 1
+
+// let text = "hello";
+// let qaytarish = text.split('').reverse().join('')
+// console.log(qaytarish);
+
+// 2
+
+// let text = "hello salom";
+// console.log(text.split(''));
+
+// 3
+
+// let text = "How are you doing today?";
+// console.log(text.length);
+
+// 4
+// let text = "salom dunyo";
+
+// let natija = text.split(" ").map(soz => soz.charAt(0).toUpperCase() + soz.slice(1)).join(" ");
+
+// console.log(natija);
